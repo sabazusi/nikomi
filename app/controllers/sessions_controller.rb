2de5f7fb009800acd6e3d16d5d8af42d44e1decc
sabazusi <kouhei.live@gmail.com>
2016-04-23 23:47:09 +0900

    redirect top after login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ActionController::Base
   def create
+    puts request.env['omniauth.auth']
+    User.find_or_create_from_auth(request.env['omniauth.auth'])
+    redirect_to root_path
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(auth)
+    provider = auth[:provider]
+    puts provider
+  end
 end
